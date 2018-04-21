dab5c1959a63b25db6b3d294b19f012d8180ec76
Anders Bjerner <abjerner@skybrud.dk>
2018-04-21 17:07:49 +0200

    Cleanup

--- src/Skybrud.Social.Twitter/TwitterUtils.cs
+++ src/Skybrud.Social.Twitter/TwitterUtils.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using Skybrud.Social.Twitter.Entities;
 using Skybrud.Social.Twitter.Entities.Formatting;
 using Skybrud.Social.Twitter.Models.Geocode;
